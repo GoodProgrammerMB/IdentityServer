eef8c8fbb1ddc5d0ed8f0b03dbc0cbe2e1f258bc
Marcin Buczak <marcin.buczak.dev@gmail.com>
2021-08-25 23:23:08 +0200

    test comit

--- GP.IdentityServer/Program.cs
+++ GP.IdentityServer/Program.cs
@@ -27,6 +27,7 @@ namespace GP.IdentityServer
                 }
                 catch (Exception ex)
                 {
+
                     // var logger = services.GetRequiredService<ILogger<Program>>();
                     // logger.LogError(ex, "An error occurred creating the DB.");
                 }
